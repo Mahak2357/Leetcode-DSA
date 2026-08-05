739b9bb8b80ebcc1df3efc5cbaff6badd3b88ff2
Mahak <mahak09822@gmail.com>
2026-08-05 16:25:52 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (63.71%) - LeetSync

---
+++ 274-h-index/h-index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(),citations.end());
+        int h=0;
+        int n=citations.size();
+
+        for(int i=0;i<n;i++){
+            int papers=n-i;
+
+            if(citations[i]>=papers){
+            h=papers;
+             break;
+            }
+        }
+return h;
+    }
+};
